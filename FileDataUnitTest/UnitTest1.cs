8564221f47882a1bbd087086db86b90d1baa1523
Anup Gupta <anup.gupta@tieto.com>
2021-08-26 17:59:08 +0530

    Check in Moq framework for unit test

--- FileDataUnitTest/UnitTest1.cs
+++ FileDataUnitTest/UnitTest1.cs
@@ -1,6 +1,7 @@
-﻿using System;
+﻿
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FileData;
+using Moq;
 
 namespace FileDataUnitTest
 {
@@ -11,6 +12,23 @@ namespace FileDataUnitTest
         public void Test_FileDetailsVersion()
         {
             BarclaysTask1 barclaysTask1 = new BarclaysTask1();
+
+            Mock<IFileDetails> mockFile = new Mock<IFileDetails>();
+
+            //I need to use Moq framework to test the random value. But I do not rememver and implemented beacause of no time left.
+
+            Assert.IsTrue(1000 == 1100);
+        }
+        [TestMethod]
+        public void Test_FileDetailsVersionAndSize()
+        {
+            BarclaysTask2 barclaysTask2 = new BarclaysTask2();
+
+            Mock<IFileDetails> mockFile = new Mock<IFileDetails>();
+
+            //I need to use Moq framework to test the random value. But I do not rememver and implemented beacause of no time left.
+
+            Assert.IsTrue(1000 == 1100);
         }
     }
 }
